520e7babf70d845d45cb43f26dbdb564bf1fb4a5
provasko <80074452+provasko@users.noreply.github.com>
2021-07-08 12:39:23 +0200

    Name and Price added to cell. Finish with first Collection view

--- VillaApp/VillaCards/RoomCollectionViewCell.swift
+++ VillaApp/VillaCards/RoomCollectionViewCell.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class RoomCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var roomImage: UIImageView!
+    @IBOutlet weak var nameRoom: UILabel!
     
+    @IBOutlet weak var priceRoom: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -22,6 +24,9 @@ class RoomCollectionViewCell: UICollectionViewCell {
     
     func setupCell(room:Room){
         self.roomImage.image = room.image
+        self.nameRoom.text = room.name
+        self.priceRoom.text = String(room.price)
+        
     }
 
 }
